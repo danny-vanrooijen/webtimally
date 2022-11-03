7988e8a923e6cd00b48d7af3de832f3fe02c70f9
Danny van Rooijen <contact@webtimally.com>
2022-11-03 13:12:30 +0100

    Updated header and footer information

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,18 +1,14 @@
 <template>
   <footer class="tpl-container py-0">
-    <div
-      class="tpl-grid border-t-2 border-grey-100 py-8 text-center sm:text-left"
-    >
-      <div
-        class="
+    <div class="tpl-grid border-t-2 border-grey-100 py-8 text-center sm:text-left">
+      <div class="
           col-span-12
           sm:col-span-6 sm:col-start-6
           gap-12
           flex flex-col
           sm:flex-row
-          justify-between
-        "
-      >
+          justify-end
+        ">
         <div class="min-w-[126px]">
           <div class="font-bold text-2xl mb-4">Links</div>
           <nav>
@@ -30,30 +26,16 @@
           <div class="tpl-menu">
             <ul class="tpl-menu">
               <li>
-                <a href="#">42-50 Kings Rd, Vaucluse NSW 2030, Australia</a>
+                <a href="#">Lindeskovvej 7<br />4330 Hvalsø</a>
               </li>
-              <li><a href="#">+7 2869 642 565</a></li>
-              <li><a href="#">info@exito.com</a></li>
+              <li><a href="tel:+4524522110">+45 24 52 21 10</a></li>
+              <li><a href="mailto:danny@webtimally.com">danny@webtimally.com</a></li>
             </ul>
           </div>
         </div>
-        <div class="min-w-[126px]">
-          <div class="font-bold text-2xl mb-4">Follow</div>
-          <nav>
-            <ul class="tpl-menu">
-              <li><a href="#">Instagram</a></li>
-              <li><a href="#">Twitter</a></li>
-              <li><a href="#">Facebook</a></li>
-              <li><a href="#">Vk</a></li>
-            </ul>
-          </nav>
-        </div>
       </div>
-      <div
-        class="col-span-12 sm:col-span-3 sm:order-first mt-12 sm:m-0 text-left"
-      >
-        <div
-          class="
+      <div class="col-span-12 sm:col-span-3 sm:order-first mt-12 sm:m-0 text-left">
+        <div class="
             font-bold
             text-4xl
             bg-gradient-to-br
@@ -62,18 +44,15 @@
             text-transparent
             bg-clip-text
             mb-16
-          "
-        >
+          ">
           Webtimally
         </div>
-        <div>Subscribe and stay up to date with our news and events.</div>
       </div>
     </div>
   </footer>
   <div class="bg-black text-white font-normal text-base">
     <div class="tpl-container py-0 flex justify-between h-20 items-center">
-      <div>Webtimally &copy; 2022</div>
-      <div><a href="#">2022 Privacy Policy</a></div>
+      <div>Webtimally &copy; 2021 - present</div>
     </div>
   </div>
 </template>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,36 +1,29 @@
 <template>
-  <header
-    class="flex justify-between h-[102px] items-center tpl-container py-0"
-  >
-    <div
-      class="
-        font-bold
-        text-4xl
-        bg-gradient-to-br
-        from-pink
-        to-purple
-        text-transparent
-        bg-clip-text
-      "
+  <header class="flex justify-between h-[102px] items-center tpl-container py-0">
+    <a
+      href="/"
+      class="w-48 font-bold text-4xl bg-gradient-to-br from-pink to-purple text-transparent bg-clip-text"
     >
       Webtimally
-    </div>
+    </a>
     <div class="hidden sm:block">
       <nav>
         <ul class="flex gap-12">
           <li><a href="/">Home</a></li>
-          <li><a href="about">About</a></li>
-          <li><a href="services">Services</a></li>
-          <li><a href="#">Blog</a></li>
+          <li><a href="about">About us</a></li>
+          <li><a href="services">Our services</a></li>
           <li><a href="#">Contact</a></li>
         </ul>
       </nav>
     </div>
-    <div class="flex items-center gap-10">
+    <div class="w-48 flex justify-end gap-10">
       <div>
-        <a class="tpl-button" href="#"
-          >Call me<i class="fa-solid fa-arrow-right hidden sm:inline-block"></i
-        ></a>
+        <a
+          class="tpl-button"
+          href="tel:+4524522110"
+        >
+          Call us<i class="fa-solid fa-arrow-right hidden sm:inline-block"></i>
+        </a>
       </div>
     </div>
   </header>
