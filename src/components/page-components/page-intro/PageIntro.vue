c22dddb6541b96c3871343f0b7f771499e7187f1
Danny van Rooijen <contact@webtimally.com>
2022-11-16 22:02:53 +0100

    Refactor to accept texts object prop

--- src/components/page-components/page-intro/PageIntro.vue
+++ src/components/page-components/page-intro/PageIntro.vue
@@ -1,22 +1,32 @@
 <template>
-  <section class="mt-4 sm:mt-8 sm:mb-24 tpl-container">
+  <section class="tpl-container mt-4 sm:mt-8 sm:mb-24">
     <div
-      class="
-        tpl-container tpl-grid
-        items-center
-        py-10
-        border-t-2 border-b-2 border-grey-100
-      "
+      class="tpl-container tpl-grid items-center border-t-2 border-b-2 border-grey-100 py-10"
     >
       <div class="col-span-9 sm:col-span-5 sm:col-start-2">
-        <h1>A little <mark>About</mark> our company</h1>
+        <h1 v-html="title"></h1>
       </div>
-      <div class="col-span-12 sm:col-span-4 sm:col-start-8 text-2xl">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt
-        </p>
+      <div class="col-span-12 text-2xl sm:col-span-4 sm:col-start-8">
+        <p>{{ description }}</p>
       </div>
     </div>
   </section>
 </template>
+
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  texts: {
+    type: Object,
+    default: {
+      title: "A little <mark>Default</mark> descriptive text",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
+    },
+  },
+});
+
+const title = computed(() => props.texts.title);
+const description = computed(() => props.texts.description);
+</script>
